fb605c8320058d4fae003276f3fe542a106a6aa2
sam <samstuff@samstuff.top>
2023-07-15 15:10:01 +0100

    Added comments to make code more readable

--- __main__.py
+++ __main__.py
@@ -1,6 +1,14 @@
+
+## IMPORTS ##
+
 import turtle, time, random, keyboard
 
+## CLASSES ##
+
 class SnakeBlock(turtle.Turtle):
+    """
+    This class represents one 30 x 30 square section of the snake
+    """
     xdirections = {0:0, 1:-30, 2:0, 3: 30}
     ydirections = {0:30, 1:0, 2:-30, 3: 0}
     def __init__(self, pos, direction, num):
@@ -18,6 +26,9 @@ class SnakeBlock(turtle.Turtle):
         #         self.mini.goto(self.pos)
         print(self)
     def next(self):
+        """
+        This method moves this piece one block forward in whatever direction it faces
+        """
         self.pos = (self.pos[0]+self.xdirections[self.direction], self.pos[1]+self.ydirections[self.direction])
         self.goto(self.pos)
         ## print(f'{self.num} is at {self.pos}')
@@ -27,6 +38,9 @@ class SnakeBlock(turtle.Turtle):
     def __repr__(self):
         return f'SnakeBlock at {self.pos} facing {self.direction}'
 class Snake:
+    """
+    This class represents the snake as a whole
+    """
     def __init__(self, length):
         self.length = length
         self.blocks = []
@@ -37,22 +51,37 @@ class Snake:
             self.blocks.append(snake)
         self.blocks.reverse()
     def left(self):
+        """
+        This method turns the snake left
+        """
         print('left')
         new = {self.blocks[-1].pos: 1}
         self.turnplaces.append(new)
     def right(self):
+        """
+        This method turns the snake right
+        """
         print('right')
         new = {self.blocks[-1].pos: 3}
         self.turnplaces.append(new)
     def up(self):
+        """
+        This method turns the snake upwards
+        """
         print('up')
         new = {self.blocks[-1].pos: 0}
         self.turnplaces.append(new)
     def down(self):
+        """
+        This method turns the snake downwards
+        """
         print('down')
         new = {self.blocks[-1].pos: 2}
         self.turnplaces.append(new)
     def checkforturns(self):
+        """
+        This method checks if any snake blocks need to turn and changes their direction if they do
+        """
         keys = [next(iter(item.items()))[0] for item in self.turnplaces]
         values = [next(iter(item.items()))[1] for item in self.turnplaces]
         # ## print(keys, values)
@@ -63,18 +92,26 @@ class Snake:
                 if self.blocks[snake.blocks.index(snakeblock)] == self.blocks[0]:
                     self.turnplaces.pop()
 
+## VARIABLES ##
+
+# Set up the screen
 screen = turtle.Screen()
 screen.setup(width=950, height=950, startx=300)
 screen.colormode(255)
 screen.bgcolor('blue')
-screen.tracer(0)  ##
+screen.tracer(0)
 
+# Set score to zero
 score = 0
-score_ttl = turtle.Turtle()
-score_ttl.penup()
-score_ttl.goto(-430, 430)
-score_ttl.color('white')
-score_ttl.hideturtle()
+
+# Set up turtle to write the score
+score_turtle = turtle.Turtle()
+score_turtle.penup()
+score_turtle.goto(-430, 430)
+score_turtle.color('white')
+score_turtle.hideturtle()
+
+# Set up turtle to represent the apple
 apple = turtle.Turtle()
 apple.shape('circle')
 apple.shapesize(1.4285714285714286,1.4285714285714286,1)
@@ -82,24 +119,33 @@ apple.color('red')
 apple.penup()
 apple.goto(random.randint(-15,15)*30, random.randint(-15,15)*30)
     
+## FUNCTIONS ##
 
 def chomp():
+    """
+    This method runs when the snake eats an apple.
+    It increases the score by one, sends the apple to a random place, and (is supposed to) increase the snake by one
+    """
     global score, snake
     score += 1
     apple.goto(random.randint(-15,15)*30, random.randint(-15,15)*30)
     pos = [snake.pos for snake in snake.blocks]
     while apple.pos in pos:
         apple.goto(random.randint(-15,15)*30, random.randint(-15,15)*30)
+    ## THE TROUBLESOME SECTION ##
     newpos = list(snake.blocks[0].pos)
     if snake.blocks[0].direction == 0: newpos[1]+=30
     elif snake.blocks[0].direction == 1: newpos[0]-=30
     elif snake.blocks[0].direction == 2: newpos[1]-=30
     elif snake.blocks[0].direction == 3: newpos[0]+=30
-    ## print(snake.blocks[0].direction)
     newblock = SnakeBlock(tuple(newpos),snake.blocks[0].direction,len(snake.blocks))
     snake.blocks.insert(0, newblock)
-    screen.update()  ##
+    screen.update()
+    
 def is_dead():
+    """
+    This method checks whether the snake is dead
+    """
     global snake
     headx = snake.blocks[-1].pos[0]
     heady = snake.blocks[-1].pos[1]
@@ -111,26 +157,32 @@ def is_dead():
         if snakeblock.distance(snake.blocks[-1]) < 5:
             return True
     return False
+
 def die():
+    """
+    This method shows the Game Over screen before quitting
+    """
     screen.bgcolor((0,0,110))
     death = turtle.Turtle()
     death.hideturtle()
     death.penup(); death.goto((-320, -50)); death.color('white'); death.write('Game Over', font=("Arial", 96, "normal"))
     apple.color(97,0,0)
-    score_ttl.clear()
-    score_ttl.penup(); score_ttl.goto(-100, -80); score_ttl.write(f'Score: {score}', font=("Arial", 36, "normal"))
+    score_turtle.clear()
+    score_turtle.penup(); score_turtle.goto(-100, -80); score_turtle.write(f'Score: {score}', font=("Arial", 36, "normal"))
     screen.update()
     time.sleep(3)
     raise SystemExit
 
+## FINISH SET-UP ##
+
 snake = Snake(4)
-screen.update()  ##
+screen.update()
 keyboard.add_hotkey('up', snake.up)
 keyboard.add_hotkey('left', snake.left)
 keyboard.add_hotkey('down', snake.down)
 keyboard.add_hotkey('right', snake.right)
-# with keyboard.Listener(on_press=report_turn) as listener:
-#         listener.join()
+
+## MAIN LOOP ##
 
 while True:
     snake.checkforturns()
@@ -141,8 +193,8 @@ while True:
         break
     if snake.blocks[-1].pos == apple.pos():
         chomp()
-    score_ttl.clear()
-    score_ttl.write(score, font=("Arial", 24, "normal"))
+    score_turtle.clear()
+    score_turtle.write(score, font=("Arial", 24, "normal"))
     screen.update()
     time.sleep(1)
     
